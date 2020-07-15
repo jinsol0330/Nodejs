1d91bf913fe18a7582d359ab644f827f22f8b0c4
DESKTOP-LNI90NK\JS <jinsol0330@naver.com>
2020-07-15 21:45:32 +0900

    App 제작_8
    글삭제 기능 완성

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var fs = require('fs');
-var url = require('url'); 
+var url = require('url');
 var qs = require('querystring');
 //url이라는 모듈을 사용할것이다 라는 것을 node.js에게 알림
 
@@ -25,48 +25,48 @@ function templateHTML(title, list, body, control) {
 function templateList(filelist) {
   var list = '<ul>';
   var i = 0;
-  while(i < filelist.length) {
+  while (i < filelist.length) {
     list = list + `<li><a href = "/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-    i = i+1;
+    i = i + 1;
   }
-  list = list+'</ul>';
+  list = list + '</ul>';
 
   return list;
 }
 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    //query의 값은 request.url에 저장함
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
+var app = http.createServer(function (request, response) {
+  var _url = request.url;
+  //query의 값은 request.url에 저장함
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
 
-    if(pathname === '/') {
-      //만약 루트라면
-      if(queryData.id === undefined) {
-        //중첩if문 사용해서 홈(root)웹페이지 구현
+  if (pathname === '/') {
+    //만약 루트라면
+    if (queryData.id === undefined) {
+      //중첩if문 사용해서 홈(root)웹페이지 구현
 
-        fs.readdir('./data', function(err, filelist) {
+      fs.readdir('./data', function (err, filelist) {
         //파일리스트를 가져오는 코드
         var title = 'Welcome';
         var description = 'Hello, Node.js';
         var list = templateList(filelist);
-        var template = templateHTML(title, list, 
+        var template = templateHTML(title, list,
           `<h2>${title}</h2>${description}`,
           `<a href="/create">create</a>`
-          );
+        );
         response.writeHead(200);
         response.end(template);
         //response.end(fs.readFileSync(__dirname + _url));
         //사용자가 접속한 url에 따라서 파일들을 읽어주는 코드
-        })
-         
-      } else {  //id값이 있다면
-        fs.readdir('./data', function(err, filelist) {
-          //파일리스트를 가져오는 코드
-        fs.readFile (`data/${queryData.id}`, 'utf8', function(err,description) {
+      })
+
+    } else {  //id값이 있다면
+      fs.readdir('./data', function (err, filelist) {
+        //파일리스트를 가져오는 코드
+        fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
           var title = queryData.id;
           var list = templateList(filelist);
-          var template = templateHTML(title, list, 
+          var template = templateHTML(title, list,
             `<h2>${title}</h2>${description}`,
             `<a href="/create">create</a> 
              <a href="/update?id=${title}">update</a>
@@ -74,59 +74,59 @@ var app = http.createServer(function(request,response){
               <input type="hidden" name="id" value="${title}">
               <input type="sumbit" value="delete">
              </form>`
-            );
+          );
           response.writeHead(200);
           response.end(template);
           //response.end(fs.readFileSync(__dirname + _url));
           //사용자가 접속한 url에 따라서 파일들을 읽어주는 코드
         });
       });
-      }
-     
-    } else if (pathname === '/create') {
-      fs.readdir('./data', function(err, filelist) {
-        //파일리스트를 가져오는 코드
-        var title = 'WEB - create';
-        var list = templateList(filelist);
-        var template = templateHTML(title, list, `
+    }
+
+  } else if (pathname === '/create') {
+    fs.readdir('./data', function (err, filelist) {
+      //파일리스트를 가져오는 코드
+      var title = 'WEB - create';
+      var list = templateList(filelist);
+      var template = templateHTML(title, list, `
           <form action = "/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p><textarea name="description" placeholder="description"></textarea></p>
             <p><input type="submit"></p>
           </form>
         `, '');
-        response.writeHead(200);
-        response.end(template);
-        //response.end(fs.readFileSync(__dirname + _url));
-        //사용자가 접속한 url에 따라서 파일들을 읽어주는 코드
-        });
+      response.writeHead(200);
+      response.end(template);
+      //response.end(fs.readFileSync(__dirname + _url));
+      //사용자가 접속한 url에 따라서 파일들을 읽어주는 코드
+    });
 
-    } else if(pathname === '/create_process') {
-        var body = '';
-        request.on('data', function(data) {
-           body = body+data;
-        });
-        //서버쪽에서 데이터를 수신할때마다 콜백함수를 호출하기로 약속되어있음
+  } else if (pathname === '/create_process') {
+    var body = '';
+    request.on('data', function (data) {
+      body = body + data;
+    });
+    //서버쪽에서 데이터를 수신할때마다 콜백함수를 호출하기로 약속되어있음
 
-        request.on('end', function() {
-          var post = qs.parse(body);
-          var title = post.title;
-          var description = post.description;
-          fs.writeFile(`data/${title}`, description, 'utf8',
-          function(err) {
-            response.writeHead(302, {Location: `/?id=${title}`});
-            //302는 페이지를 다른 곳으로 리다이렉션 시키라는 의미
-            response.end();
-          })
-        });
-    } else if(pathname === '/update') {
-        fs.readdir('./data', function(err, filelist) {
-        //파일리스트를 가져오는 코드
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err,description) {
-          var title = queryData.id;
-          var list = templateList(filelist);
-          var template =  templateHTML(title, list, 
-            `
+    request.on('end', function () {
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+      fs.writeFile(`data/${title}`, description, 'utf8',
+        function (err) {
+          response.writeHead(302, { Location: `/?id=${title}` });
+          //302는 페이지를 다른 곳으로 리다이렉션 시키라는 의미
+          response.end();
+        })
+    });
+  } else if (pathname === '/update') {
+    fs.readdir('./data', function (err, filelist) {
+      //파일리스트를 가져오는 코드
+      fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+        var title = queryData.id;
+        var list = templateList(filelist);
+        var template = templateHTML(title, list,
+          `
             <form action = "/update_process" method="post">
             <input type="hidden" name="id" value="${title}">
             <p><input type="text" name="title" placeholder="title" value="${title}"></p>
@@ -134,40 +134,53 @@ var app = http.createServer(function(request,response){
             <p><input type="submit"></p>
             </form>
             `,
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-           );
-         response.writeHead(200);
-         response.end(template);
-         //response.end(fs.readFileSync(__dirname + _url));
-         //사용자가 접속한 url에 따라서 파일들을 읽어주는 코드
-       });
-     });
-    } else if(pathname === '/update_process') {
-      var body = '';
-      request.on('data', function(data) {
-         body = body+data;
+          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+        );
+        response.writeHead(200);
+        response.end(template);
+        //response.end(fs.readFileSync(__dirname + _url));
+        //사용자가 접속한 url에 따라서 파일들을 읽어주는 코드
       });
-      //서버쪽에서 데이터를 수신할때마다 콜백함수를 호출하기로 약속되어있음
+    });
+  } else if (pathname === '/update_process') {
+    var body = '';
+    request.on('data', function (data) {
+      body = body + data;
+    });
+    //서버쪽에서 데이터를 수신할때마다 콜백함수를 호출하기로 약속되어있음
 
-      request.on('end', function() {
-        var post = qs.parse(body);
-        var id = post.id;
-        var title = post.title;
-        var description = post.description;
-        fs.rename(`data/${id}`, `data/${title}`, function(error) {
-          fs.writeFile(`data/${title}`, description, 'utf8',
-        function(err) {
-          response.writeHead(302, {Location: `/?id=${title}`});
-          response.end();
-        })
-        })
-        
-      });
+    request.on('end', function () {
+      var post = qs.parse(body);
+      var id = post.id;
+      var title = post.title;
+      var description = post.description;
+      fs.rename(`data/${id}`, `data/${title}`, function (error) {
+        fs.writeFile(`data/${title}`, description, 'utf8',
+          function (err) {
+            response.writeHead(302, { Location: `/?id=${title}` });
+            response.end();
+          })
+      })
+    });
 
-    } else {
-        //루트상태가 아니라면 error화면 출력
-        response.writeHead(404);
-        response.end('Not Found');
-    }
+  } else if (pathname === '/delete_process') {
+    var body = '';
+    request.on('data', function (data) {
+      body = body + data;
+    });
+
+    request.on('end', function () {
+      var post = qs.parse(body);
+      var id = post.id;
+      fs.unlink(`data/${id}`, function (error) {
+        response.writeHead(302, { Location: `/` });
+        response.end();
+      })
+    });
+  } else {
+    //루트상태가 아니라면 error화면 출력
+    response.writeHead(404);
+    response.end('Not Found');
+  }
 });
 app.listen(3000);
