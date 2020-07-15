d46db57113ebca1470ff0c1697604db49ee5cc06
DESKTOP-LNI90NK\JS <jinsol0330@naver.com>
2020-07-15 22:48:24 +0900

    App 제작_10
    모듈의 활용

--- main.js
+++ main.js
@@ -4,37 +4,7 @@ var url = require('url');
 var qs = require('querystring');
 //url이라는 모듈을 사용할것이다 라는 것을 node.js에게 알림
 
-//객체 생성
-var template = {
-  HTML:function (title, list, body, control) {
-    return `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            ${control}
-            ${body}
-          </body>
-          </html>
-          `;
-  },
-  list:function(filelist) {
-    var list = '<ul>';
-    var i = 0;
-    while (i < filelist.length) {
-      list = list + `<li><a href = "/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-      i = i + 1;
-    }
-    list = list + '</ul>';
-
-    return list;
-  }
-}
+var template = require('./lib/template.js');
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
@@ -186,3 +156,4 @@ var app = http.createServer(function (request, response) {
   }
 });
 app.listen(3000);
+
